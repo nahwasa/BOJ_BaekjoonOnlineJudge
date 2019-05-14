e47a956a87160f2f72a73a2f6ae1daafda6280a5
Se-young Hwang <49299283+NaHwaSa@users.noreply.github.com>
2019-05-14 11:30:47 +0900

    Commit BOJ_10826.java
    1. 메모리를 써서 피보나치의 중간결과를 저장하지 않으면 시간초과.
       -> 중간 계산 결과를 배열에 저장하며 진행
    2. 피보나치 n=10000까지 계산이 되야하므로 일반적인 int로는 오버플로우. 
       -> 자바의 경우 BigInteger 사용

---
+++ BOJ_10826.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {	
+	private static BigInteger[] fiboMem;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		fiboMem = new BigInteger[ (n <= 1)? 2 : n + 1];
+		fiboMem[0] = new BigInteger("0");
+		fiboMem[1] = new BigInteger("1");		
+
+		bw.write(fibo(n).toString() + "\n");
+		bw.flush();
+		br.close();
+		bw.close();		
+	}
+	
+	private static BigInteger fibo(int n) {		
+		if (fiboMem[n] != null)
+			return fiboMem[n];
+		
+		BigInteger result = fibo(n-2).add(fibo(n-1));
+		if (fiboMem[n] == null)
+			fiboMem[n] = result;
+		
+		return result;
+	}
+}
