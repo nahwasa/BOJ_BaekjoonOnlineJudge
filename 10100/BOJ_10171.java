bf54ea891ef54b62c52df87f68be6d2af820df64
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:34:24 +0900

    고양이

---
+++ 10100/BOJ_10171.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("\\    /\\" + "\n");
+		bw.write(" )  ( ')" + "\n");
+		bw.write("(  /  )" + "\n");
+		bw.write(" \\(__)|" + "\n");
+		
+		
+		bw.flush();
+		br.close();
+		bw.close();		
+	}	
+}
