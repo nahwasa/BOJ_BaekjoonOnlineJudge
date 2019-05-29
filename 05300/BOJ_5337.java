9cd4e6860f7f24000643968840a96b738e2eaa18
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:24:47 +0900

    웰컴

---
+++ 05300/BOJ_5337.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write(".  .   ." + "\n");
+		bw.write("|  | _ | _. _ ._ _  _" + "\n");
+		bw.write("|/\\|(/.|(_.(_)[ | )(/." + "\n");
+
+		
+		bw.flush();
+		br.close();
+		bw.close();		
+	}	
+}
