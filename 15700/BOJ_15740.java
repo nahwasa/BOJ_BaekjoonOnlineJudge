699dad3eb6b1c2f7bf9c143595e5cbe615e6638d
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:58:11 +0900

    A + B - 9

---
+++ 15700/BOJ_15740.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		
+		BigInteger sum = a.add(b);
+		
+		bw.write(sum.toString() + "\n");
+		
+		bw.flush();
+		br.close();
+		bw.close();		
+	}	
+}
