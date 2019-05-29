755a495b9c63105e65a1f85ce42d936304fe7717
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:43:56 +0900

    A + B - 4

---
+++ 10900/BOJ_10951.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			if(0 < a && a < 10 && b > 0 && b < 10) {
+				System.out.println(a+b);				
+			}
+		}
+	}
+}
