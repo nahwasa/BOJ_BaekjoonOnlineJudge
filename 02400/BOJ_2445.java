6ba28273e2e4087fb0ff5fa62719b3b13d3f5126
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 10:51:04 +0900

    별 찍기 - 8

---
+++ 02400/BOJ_2445.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i = n; i >= 1; i--) {
+			for (int j = n - i; j >= 0; j--) {
+				bw.write("*");
+			}
+			
+			for (int j = 1; j < 2 * i - 1; j++) {
+				bw.write(" ");
+			}			
+			
+			for (int j = n - i; j >= 0; j--) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = n - i; j > 0; j--) {
+				bw.write("*");
+			}
+			
+			for (int j = 0; j <= 2 * i - 1; j++) {
+				bw.write(" ");
+			}			
+			
+			for (int j = n - i; j > 0; j--) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		
+		
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
