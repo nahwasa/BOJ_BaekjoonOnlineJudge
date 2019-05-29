4f5aec3596a552fa853c166a562af8100d95d83d
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:35:23 +0900

    나머지

---
+++ 10400/BOJ_10430.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		//첫째 줄에 (A+B)%C, 둘째 줄에 (A%C + B%C)%C, 셋째 줄에 (A×B)%C, 넷째 줄에 (A%C × B%C)%C를 출력한다.
+		
+		
+		System.out.println((a+b)%c);
+		System.out.println((a%c + b%c)%c);
+		System.out.println((a*b)%c);
+		System.out.println(a%c * b%c);
+		
+	}
+}
