d916e7c391ab356c64f9dff9d134720157909e02
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:51:23 +0900

    그대로 출력하기 2

---
+++ 11700/BOJ_11719.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while (sc.hasNext()) {
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+				
+	}
+}
