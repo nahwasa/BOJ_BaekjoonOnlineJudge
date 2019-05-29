e8971022865d00432f609c544811bda5d2e314c3
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:34:46 +0900

    개

---
+++ 10100/BOJ_10172.java
@@ -0,0 +1,10 @@
+public class Main{
+	
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
