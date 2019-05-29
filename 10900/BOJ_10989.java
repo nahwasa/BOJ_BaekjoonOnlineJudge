523b7e2d7dc58eb1a54086396a6d1bead1c36d71
Se-young Hwang <nahwasa@gmail.com>
2019-05-29 11:45:09 +0900

    수 정렬하기 3

---
+++ 10900/BOJ_10989.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+				
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < n; i++) {
+			bw.write(arr[i] + "\n");
+		}
+		
+		bw.flush();
+		br.close();
+		bw.close();		
+	}	
+}
