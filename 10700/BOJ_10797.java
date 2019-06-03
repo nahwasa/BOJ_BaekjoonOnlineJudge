7589f8dffb5afc1c0861f8bc020f6290cd7602ec
Se-young Hwang <nahwasa@gmail.com>
2019-06-03 13:55:02 +0900

    10부제

---
+++ 10700/BOJ_10797.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String n = br.readLine();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int cnt = 0;
+		while(st.hasMoreTokens())
+			if (st.nextToken().equals(n))
+				cnt++;
+		bw.write(cnt+"\n");
+		
+		bw.flush();		
+		bw.close();
+		br.close();
+	}
+}	
