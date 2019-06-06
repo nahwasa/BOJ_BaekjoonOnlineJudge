e27584e86b631faffc5c6aba3acc393540bf2fc0
Se-young Hwang <nahwasa@gmail.com>
2019-06-06 15:42:13 +0900

    문자열 잘라내기
    문자열을 90도로 눕힌다음 연산함.
    동일 문자열 찾는 방식은 역시 해쉬가 최고!
    배열에 넣고 검사하면 O(N)이라 분명 시간초과 뜰꺼

---
+++ 02800/BOJ_2866.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());		
+		char[][] arr = new char[M][N];
+	
+		for (int i = 0; i < N; i++) {
+			String tmp = br.readLine();
+			for (int j = 0; j < M; j++) {
+				arr[j][i] = tmp.charAt(j);				
+			}
+		}
+		
+		int cnt = 0;
+		for (int i = 1; i < N; i++) {
+			HashSet<String> hs = new HashSet<String>();
+			for (int k = 0; k < M; k++) {
+				String tmp = String.copyValueOf(arr[k], i, N-i);
+				if (hs.contains(tmp)) {
+					bw.write(cnt + "\n");
+					bw.flush();		
+					bw.close();
+					br.close();
+					return;
+				}
+				hs.add(tmp);
+			}			
+			cnt++;
+		}
+		
+		bw.write(cnt + "\n");
+		
+		
+		bw.flush();		
+		bw.close();
+		br.close();
+	}
+}
