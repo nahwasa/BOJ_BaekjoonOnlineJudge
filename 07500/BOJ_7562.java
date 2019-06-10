a34733384597f1e3c5910099119243d18b3ce559
Se-young Hwang <nahwasa@gmail.com>
2019-06-10 14:10:01 +0900

    나이트의 이동
    BFS를 이용한 최단거리 찾기 문제

--- 07500/BOJ_7562.java
+++ 07500/BOJ_7562.java
@@ -6,7 +6,7 @@ import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
