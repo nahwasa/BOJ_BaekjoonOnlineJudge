7adc4ae519c6cdc3591a0e280310a77e1451805c
Se-young Hwang <nahwasa@gmail.com>
2019-06-12 15:36:24 +0900

    두 수 비교하기
    할말이 없음

---
+++ 01300/BOJ_1330.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		bw.write(A==B?"==\n":A>B?">\n":"<\n");
+		bw.flush();
+		br.close();
+		bw.close();		
+	}
+}
