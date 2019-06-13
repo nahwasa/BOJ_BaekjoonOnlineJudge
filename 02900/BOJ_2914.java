90202e09d799f9b7321c15cf16d6d2ee227547ed
Se-young Hwang <nahwasa@gmail.com>
2019-06-13 17:44:11 +0900

    저작권

---
+++ 02900/BOJ_2914.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw.write((Integer.parseInt(st.nextToken()) * (Integer.parseInt(st.nextToken())-1) + 1) + "\n");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
