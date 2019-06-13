7610d15b74c624dae99cce7526d9b01b0320b7b5
Se-young Hwang <nahwasa@gmail.com>
2019-06-13 17:57:50 +0900

    R2

---
+++ 03000/BOJ_3046.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		bw.write((2*b-a) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
