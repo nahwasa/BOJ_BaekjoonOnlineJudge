386424eb8d03a6c06cf966b700027b31be48879f
Se-young Hwang <nahwasa@gmail.com>
2019-06-13 16:40:35 +0900

    행렬 덧셈

---
+++ 02700/BOJ_2738.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				arr[i][j] += Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int i = 0; i < N; i++) {			
+			for (int j = 0; j < M; j++) {
+				bw.write(arr[i][j] + " ");
+			}
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
