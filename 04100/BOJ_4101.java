6cd1ed3c8284f5672c9a603ff2a1b5d1c634f57d
Se-young Hwang <nahwasa@gmail.com>
2019-06-14 13:37:43 +0900

    크냐?

---
+++ 04100/BOJ_4101.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+				
+		while(true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a == 0 && b == 0)
+				break;
+			bw.write(a>b?"Yes\n":"No\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}	
+}
