914dec6956ff17adf9d655872d2148224a3b80f1
Se-young Hwang <nahwasa@gmail.com>
2019-06-14 10:29:18 +0900

    캥거루 세마리
    초등수학문제.
    결국 둘 중 큰 공간 -1 만큼 점프됨.

---
+++ 02900/BOJ_2965.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		bw.write(String.valueOf(C-B>B-A?C-B-1:B-A-1) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}	
