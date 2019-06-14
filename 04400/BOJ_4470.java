b37187074e0e51f3793cf83ddb71a4e15ffd12b2
Se-young Hwang <nahwasa@gmail.com>
2019-06-14 15:52:19 +0900

    줄번호

---
+++ 04400/BOJ_4470.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= N; i++)
+			bw.write(i + ". " + br.readLine() + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
