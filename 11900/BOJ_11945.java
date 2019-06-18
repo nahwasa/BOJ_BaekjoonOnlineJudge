87e0050178d715c8f41d505f66952ce4f5472eb6
Se-young Hwang <nahwasa@gmail.com>
2019-06-18 15:28:34 +0900

    뜨거운 붕어빵

---
+++ 11900/BOJ_11945.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		while (N-->0)
+			bw.write(new StringBuilder(br.readLine()).reverse().toString() + "\n");		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
