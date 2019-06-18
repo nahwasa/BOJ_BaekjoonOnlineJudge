aa883b816ee280fa6d9084c92f1b820598e57326
Se-young Hwang <nahwasa@gmail.com>
2019-06-18 15:31:52 +0900

    파일 옮기기

---
+++ 11900/BOJ_11943.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		b += Integer.parseInt(st.nextToken());
+		a += Integer.parseInt(st.nextToken());
+		bw.write((a>b?b:a) + "\n");		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
