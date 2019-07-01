e2905a067c41b23bbae1d3fd3d5dc0a92a33effc
Se-young Hwang <nahwasa@gmail.com>
2019-07-01 13:29:03 +0900

    치킨 두 마리 (...)
    음.. 뭐..

---
+++ 14400/BOJ_14489.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(br.readLine());
+		int result = A+B-2*C;
+		bw.write(String.format("%d\n", result<0?A+B:result));		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
