2f241c21238769d418b597224d94bf5b9e139f95
Se-young Hwang <nahwasa@gmail.com>
2019-07-02 13:56:37 +0900

    손익분기점
    수학!

---
+++ 01700/BOJ_1712.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		if (B>=C)
+			bw.write("-1\n");			
+		else
+			bw.write(String.format("%d\n", A/(C-B)+1));		
+		bw.flush();
+		bw.close();
+		br.close();
+	}	
+}
