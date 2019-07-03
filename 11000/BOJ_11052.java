a9b483ea26c4b229db04c0d5def7c56a54d07fc1
Se-young Hwang <nahwasa@gmail.com>
2019-07-03 09:43:51 +0900

    카드 구매하기
    N개 구매 시 가장 높은가격으로 구매하는 법
    1. N개를 그냥 구매
    2. N-1개 구매시 가장 높은 가격 + 1개 구매시 가장 높은 가격
    3. N-2개 구매시 가장 높은 가격 + 2개 구매시 가장 높은 가격
    ...
    4. N/2개 구매시 가장 높은 가격 + N/2개 구매시 가장 높은 가격 (홀수는 정수형 소수점 날림으로 처리됨)
    이들중 가장 높은 가격이 됨.
    즉 DP로 처리 가능하고, i/2까지 각 쌍을 더해보면서 max 수치를 찾으면 됨.

--- 11000/BOJ_11052.java
+++ 11000/BOJ_11052.java
@@ -11,19 +11,16 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N+1];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) 
-			arr[i] = Integer.parseInt(st.nextToken());
-		int[] dp = new int[N+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());		
 		for (int i = 1; i <= N; i++) {
-			int max = arr[i];
-			for (int j = i-1; j >= 1; j--) {
-				int tmp = dp[j] + arr[i-j];
+			int max = arr[i] = Integer.parseInt(st.nextToken());
+			for (int j = i/2; j >= 1; j--) {
+				int tmp = arr[j] + arr[i-j];
 				if (tmp > max) max = tmp;
 			}
-			dp[i] = max;
+			arr[i] = max;
 		}	
-		bw.write(dp[N] + "\n");
+		bw.write(arr[N] + "\n");
 		bw.flush();
 		br.close();
 		bw.close();		
