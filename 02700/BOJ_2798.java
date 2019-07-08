e73e8b0722e7cb3ea6a3d9ea8819f7bf6af23559
Se-young Hwang <nahwasa@gmail.com>
2019-07-08 12:49:38 +0900

    블랙잭
    이지

---
+++ 02700/BOJ_2798.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(arr);
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i+1; j < N; j++) {
+				for (int k = j+1; k < N; k++) {
+					int sum = arr[i]+arr[j]+arr[k];
+					if (sum > M) break;
+					if (sum == M) {
+						bw.write(M + "\n");
+						bw.flush();
+						return;
+					}
+					if (sum > max) max = sum;
+				}
+			}
+		}
+		bw.write(max + "\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
