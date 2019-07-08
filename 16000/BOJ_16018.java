bac125bcc50efeb7da17ca5422a88a739f66aac2
Se-young Hwang <nahwasa@gmail.com>
2019-07-08 16:49:16 +0900

    Occupy parking
    이지

---
+++ 16000/BOJ_16018.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		int N = Integer.parseInt(br.readLine());
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+		int cnt = 0;
+		for (int i = 0; i < N; i++)
+			if(s1.charAt(i)=='C' && s2.charAt(i)=='C') cnt++;
+		bw.write(cnt+"\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}	
+}
