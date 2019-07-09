fbddef00b65d3b91eb3afa5a678ed9cbca063ca1
Se-young Hwang <nahwasa@gmail.com>
2019-07-09 11:38:48 +0900

    최소공배수

---
+++ 13200/BOJ_13241.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long a = Integer.parseInt(st.nextToken());
+		long b = Integer.parseInt(st.nextToken());
+		long mul = a*b;
+		long r = -1;
+		while (r!=0) {
+			r = a%b;
+			a = b;
+			b = r;
+		}
+		bw.write((mul/a)+"\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}	
+}
