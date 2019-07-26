0f0e4112d45ceef49e030473951f098d3a4cd4a8
Se-young Hwang <nahwasa@gmail.com>
2019-07-26 19:47:24 +0900

    닉네임에 갓 붙이기
    음절도 직접 나누라고 했으면 더 좋았을텐데.. 넘 쉬움

---
+++ 13100/BOJ_13163.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int TC = Integer.parseInt(br.readLine());
+		while (TC-->0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			bw.write("god");
+			while (st.hasMoreTokens())
+				bw.write(st.nextToken());
+			bw.newLine();
+		}
+		bw.close();
+		br.close();
+	}
+}
