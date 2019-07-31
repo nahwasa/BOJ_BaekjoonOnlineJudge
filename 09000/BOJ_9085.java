e869f58097b06013f555c5fb0dedf1e43eb723af
Se-young Hwang <nahwasa@gmail.com>
2019-07-31 12:35:34 +0900

    더하기
    이지

---
+++ 09000/BOJ_9085.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int TC = Integer.parseInt(br.readLine());
+		while (TC-->0) {
+			br.readLine();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int sum = 0;
+			while (st.hasMoreTokens()) sum+=Integer.parseInt(st.nextToken());
+			bw.write(sum+"\n");
+		}
+		bw.flush();
+		br.close();
+		bw.close();		
+	}
+}
