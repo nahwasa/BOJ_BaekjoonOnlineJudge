9ecfdf4f2120f4e3d368ba27c985411d905442be
Se-young Hwang <nahwasa@gmail.com>
2019-08-05 18:16:27 +0900

    다면체
    이지

---
+++ 10500/BOJ_10569.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int TC = Integer.parseInt(br.readLine());
+		while (TC-->0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			bw.write(String.valueOf(2 - Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+"\n");
+		}		
+		bw.flush();
+		br.close();
+		bw.close();		
+	}
+}
