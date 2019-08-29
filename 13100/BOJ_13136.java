ebb2bf6a0bf1c21bd59edf7474d3ae15db18611a
Se-young Hwang <nahwasa@gmail.com>
2019-08-29 12:41:30 +0900

    Do Not Touch Anything
    1000000 1000000 1
    이럴 경우를 대비해 long형으로 계산해야된다는점만 주의

---
+++ 13100/BOJ_13136.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		long calc = (long)(R/N+(R%N!=0?1:0)) * (C/N+(C%N!=0?1:0));
+		bw.write(calc + "\n");
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
