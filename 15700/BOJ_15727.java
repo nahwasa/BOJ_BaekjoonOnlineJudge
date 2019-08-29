8ee08942bdf3dfca57e3851e6270f2d0671f64d2
Se-young Hwang <nahwasa@gmail.com>
2019-08-29 11:33:08 +0900

    조별과제를 하려는데 조장이 사라졌다
    이지

---
+++ 15700/BOJ_15727.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		bw.write(N/5 + (N%5!=0?1:0) + "\n");		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
