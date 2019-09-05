3af4228601ebbe135e2e14b560938679164c1449
Se-young Hwang <nahwasa@gmail.com>
2019-09-05 12:54:49 +0900

    이칙연산
    쉬운거긴한데 주의점은
    A/B*C를 하면 안되고, A*C/B를 해야함.
    A/B에서 소수점 날아가고 C가 곱해지기 때문에.
    A*C 한다음 B로 나눠야 소수점 날리는게 제대로 됨.

---
+++ 15700/BOJ_15726.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		long tmp = (long)A*B/C;
+		tmp = Math.max(tmp, (long)A*C/B);
+		bw.write(tmp + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}	
+}
