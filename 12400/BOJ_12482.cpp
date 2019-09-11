63efc9f93266408c5c9f695f375161ed961034f0
Se-young Hwang <nahwasa@gmail.com>
2019-09-11 23:00:50 +0900

    Mystery Square (Small) - C++
    Large 해보면서 자꾸 시간초과나서 C++로 짠거 Small에서 시간차이 얼마나 나는지 보려고 짜봄.
    설명은 자바쪽 글에 있습니다.
    자바쪽이랑 동일한 코드임에도 무려 3초나 차이나고(자바 7초, c++ 4초) 메모리도 8~9배정도 자바가 더 많이쓰네요 -_-;;

---
+++ 12400/BOJ_12482.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+   cin.tie(NULL);
+   ios_base::sync_with_stdio(false);
+   
+   int tc;
+   cin >> tc;
+   for (int i = 1; i <= tc; i++) {
+      string s;
+      cin >> s;
+      long long bit0Chk = 0;
+      long long bit1Chk = 0;
+      for (int i = 0; i < s.length(); i++) {
+         switch(s.at(i)) {
+         case '0' :
+            bit0Chk |= 1l << (s.length()-1-i);
+            break;
+         case '1' :
+            bit1Chk |= 1l << (s.length()-1-i);
+            break;
+         default :
+            // ?인경우 -> 처리안함
+            break;
+         }
+      }
+      long long tmp = 1;
+      for (long long l = 1; ; l++) {
+         tmp = l * l;
+         if ((tmp&bit0Chk) == 0 && (tmp&bit1Chk) == bit1Chk)
+            break;
+      }
+      string sTmp;
+      for (int i = 0; i < s.length(); i++) {
+      	sTmp.insert(0, (tmp&1)==1?"1":"0");
+      	tmp>>=1;
+      }
+      cout << "Case #" << i << ": " << sTmp << endl;
+   }
+   return 0;
+}
