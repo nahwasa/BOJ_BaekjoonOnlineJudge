1051b8472c164f1aed456bb1b6073607870e6388
Se-young Hwang <nahwasa@gmail.com>
2019-09-20 11:08:37 +0900

    최솟값과 최댓값
    세그먼트 트리 응용

--- 02300/BOJ_2357.java
+++ 02300/BOJ_2357.java
@@ -6,24 +6,21 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int init(boolean type, int[] arr, int[][] seg, int n, int s, int e) {
-		if (type) {
-			return s==e?(seg[n][0]=arr[s]):(seg[n][0] = Math.min(init(type,arr,seg,n*2,s,(s+e)/2), init(type,arr,seg,n*2+1,(s+e)/2+1,e)));
-		} else {
-			return s==e?(seg[n][1]=arr[s]):(seg[n][1] = Math.max(init(type,arr,seg,n*2,s,(s+e)/2), init(type,arr,seg,n*2+1,(s+e)/2+1,e)));
-		}		
+	private static SegNode init(int[] arr, SegNode[] seg, int n, int s, int e) {
+		if (s==e)
+			return seg[n]=new SegNode(arr[s]);
+		SegNode sn1 = init(arr,seg,n*2,s,(s+e)/2);
+		SegNode sn2 = init(arr,seg,n*2+1,(s+e)/2+1,e);
+		return seg[n] = new SegNode(Math.min(sn1.min, sn2.min), Math.max(sn1.max, sn2.max));
 	}
 	
-	private static int min(int[][] seg, int n, int s, int e, int l, int r) {
-		if (l>e || r<s) 	return Integer.MAX_VALUE;
-		if (l<=s && e<=r) 	return seg[n][0];
-		return Math.min(min(seg,n*2,s,(s+e)/2,l,r), min(seg,n*2+1,(s+e)/2+1,e,l,r));
-	}
-	
-	private static int max(int[][] seg, int n, int s, int e, int l, int r) {
-		if (l>e || r<s) 	return Integer.MIN_VALUE;
-		if (l<=s && e<=r) 	return seg[n][1];
-		return Math.max(max(seg,n*2,s,(s+e)/2,l,r), max(seg,n*2+1,(s+e)/2+1,e,l,r));
+	private static int[] result(SegNode[] seg, int n, int s, int e, int l, int r) {
+		if (l>e || r<s) 	return new int[] {Integer.MAX_VALUE, Integer.MIN_VALUE};
+		if (l<=s && e<=r) 	return new int[] {seg[n].min, seg[n].max};
+		int[] sn1 = result(seg,n*2,s,(s+e)/2,l,r);
+		int[] sn2 = result(seg,n*2+1,(s+e)/2+1,e,l,r);
+		
+		return new int[] {Math.min(sn1[0], sn2[0]), Math.max(sn1[1], sn2[1])};
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -34,19 +31,30 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int h = (int)Math.ceil(Math.log(N) / Math.log(2));
 		int[] arr = new int[N];
-		int[][] seg = new int[1<<(h+1)][2];
+		SegNode[] seg = new SegNode[1<<(h+1)];
 		for (int i = 0; i < N; i++)
 			arr[i] = Integer.parseInt(br.readLine());
-		init(true, arr, seg, 1, 0, N-1);
-		init(false, arr, seg, 1, 0, N-1);
+		init(arr, seg, 1, 0, N-1);
 		while (M-->0) {
 			st = new StringTokenizer(br.readLine());
 			int l = Integer.parseInt(st.nextToken()) - 1;
 			int r = Integer.parseInt(st.nextToken()) - 1;
-			bw.write(min(seg, 1, 0, N-1, l, r) + " " + max(seg, 1, 0, N-1, l, r) + "\n");
+			int[] result = result(seg, 1, 0, N-1, l, r);
+			bw.write(result[0] + " " + result[1] + "\n");
 		}
 		bw.flush();
 		bw.close();
 		br.close();
+	}	
+}
+
+class SegNode {
+	int min, max;
+	public SegNode(int min, int max) {
+		this.min = min;
+		this.max = max;
+	}	
+	public SegNode(int num) {
+		min=max=num;
 	}	
 }
