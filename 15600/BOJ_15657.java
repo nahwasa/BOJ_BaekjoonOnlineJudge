f36e8f2879ea853e38f8af9d1dc6d9b13a9370b6
Se-young Hwang <nahwasa@gmail.com>
2021-01-19 15:04:01 +0900

    N과 M(8)

---
+++ 15600/BOJ_15657.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+	static int M;
+	static int[] arr;
+	
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		int i = 0;
+		while (st.hasMoreTokens())
+			arr[i++] = Integer.parseInt(st.nextToken());
+		Arrays.parallelSort(arr);
+		solve(0, new LinkedList<Integer>(), 0);
+		bw.write(sb.toString());
+		bw.flush();
+		br.close();
+		bw.close();		
+	}
+	
+	private static void solve(int cnt, List<Integer> sel, int s) {
+		if (cnt == M) {
+			for (int tmp : sel) {
+				sb.append(tmp);
+				sb.append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = s; i < arr.length; i++) {
+			sel.add(arr[i]);
+			solve(cnt+1, sel, i);
+			sel.remove(sel.size()-1);
+		}
+	}
+}
