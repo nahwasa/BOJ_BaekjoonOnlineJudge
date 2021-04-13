9ca36950714719a2d567e975c25bea95b2529c0e
Se-young Hwang <nahwasa@gmail.com>
2021-04-13 13:44:17 +0900

    Bottle Return

---
+++ 21300/BOJ_21300.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int sum = 0;
+        while (st.hasMoreTokens()) sum += Integer.parseInt(st.nextToken()) * 5;
+        System.out.println(sum);
+    }
+}
