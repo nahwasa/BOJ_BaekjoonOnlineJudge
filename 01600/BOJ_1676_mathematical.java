0e24087bd6f0c00acc9cf65da2b8dc868bd8c9bb
Se-young Hwang <nahwasa@gmail.com>
2021-04-26 14:38:46 +0900

    팩토리얼 0의 개수 (수학적인 방법 버전)
    결국 2 x 5의 쌍을 많이 찾아내면 됨.
    10의 배수도 2 x 5의 쌍에 들어가니 포함되고..
    어찌됬든 1부터 n까지 다 곱하다보면
    2의 배수가 5의 배수보다 무조건 많아지므로, 소인수분해 시 5의 갯수만 세면 되므로 코드가 성립합.

---
+++ 01600/BOJ_1676_mathematical.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while (n > 0) cnt += (n/=5);
+        System.out.println(cnt);
+    }
+}
