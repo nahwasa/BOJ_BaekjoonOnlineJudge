99aa0e309894dc419e8694e536d62d23c9f0f3f8
Se-young Hwang <nahwasa@gmail.com>
2021-04-28 13:10:09 +0900

    경로 찾기
    플로이드 와샬 혹은 그냥 bfs로 전부 해보면 됨.

---
+++ 11400/BOJ_11403.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = st.nextToken().charAt(0) == '1' ? true : false;
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (arr[i][j])  continue;
+                    if (arr[i][k] && arr[k][j])
+                        arr[i][j] = true;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(arr[i][j] ? 1 : 0).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
