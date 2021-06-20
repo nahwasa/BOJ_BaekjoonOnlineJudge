bea4d80f6987719300f54c492de6935d6e8e0c71
Se-young Hwang <nahwasa@gmail.com>
2021-06-20 18:09:32 +0900

    Googlander (Small)
    1. 첫 생각은 완탐이라 생각했는데, 10x10 크기에다가 TC도 있으니 무리여서 (각 칸마다 2가지 방법으로 분기해야 하니깐) DP로 생각해봄.
    2. 결론은, 예를들어 3X3의 가지수를 아래와 같이 나눠서 생각함.
    2.1 아래서부터 2개의 행의 가지수
    2.2 우측에서부터 2개의 열의 가지수
    그리고 3x3의 가지수는 2.1에서 나온 수 + 2.2에서 나온 수.
    3. 왜냐하면 우선 2x3 크기의 경우의 수를 구했다치고, 3x3으로 확장된다면 2x3 크기에서 나온 가지수를 그대로 다 가져가고 거기서 3x3의 좌측상단에서는 그냥 우측으로 턴만 해줌. 그리고 거기서 뻗어나온 가지수는 결국 3x2 크기에서 나온 가지수와 동일함.
    4. 이걸 재귀적으로 생각해보면 아래와 같음
    4.1. 1x1 크기일 때 : 자명하게 1가지
    [1]
    4.2. 2x2 크기로 확장 -> 우선 1x2와 2x1을 생각해봄.
    { ?, 1 }
    { 1, [1]}
    그럼 저 ? 부분(2x2의 총합)은 우선 1행밖에 없을 때의 가지수 + ? 부분에서 우측으로 턴하고, 나머지는 1열밖에 없을때의 가지수와 동일하므로
    ? = 2
    4.3 이번엔 3x3으로 확장.
    ?  ?  ?
    ? {2 1}
    ? {1[1]}
    이 때도 마찬가지로, 모든 ?에 대해 같은 방법으로 해주게 되면
    ? 3 1
    3 2 1
    1 1 1
    이렇게 되고, 그럼 ? 부분은
    3 2 1
    1 1 1
    요거랑
    3 1
    2 1
    1 1
    요거의 갯수를 합친것과 동일하므로 3 + 3 = 6

---
+++ 12100/BOJ_12180.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int t = 1; t <= tc; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int[][] arr = new int[r][c+1];
+            Arrays.fill(arr[r-1], 1);
+            for (int i = r-2; i >= 0; i--) {
+                for (int j = c-1; j >= 0; j--) {
+                    arr[i][j] = arr[i][j+1] + arr[i+1][j];
+                }
+            }
+            sb.append("Case #").append(t).append(": ").append(arr[0][0]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
