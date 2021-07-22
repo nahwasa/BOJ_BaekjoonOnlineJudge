b12c9f707e2d933e2860bbf91cb0469da286dc9a
Se-young Hwang <nahwasa@gmail.com>
2021-07-22 21:12:26 +0900

    전체 계산 횟수
    그냥 사칙연산

---
+++ 17100/BOJ_17174.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int sum = n;
+        while (n >= m) {
+            n /= m;
+            sum += n;
+        }
+        System.out.println(sum);
+    }
+}
