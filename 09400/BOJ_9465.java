cb938c698801335888b8cc0f574a02acf56ecbb2
Se-young Hwang <nahwasa@gmail.com>
2021-08-09 21:08:15 +0900

    스티커
    스티커 위치가 아래와 같다고 보자.
    A C E
    B D F
    이 때, 각 스티커의 가치 중 음수인 것은 없으므로 결국 우측으로 갈수록 무조건 같거나 증가한다!
    전형적인 DP를 활용한 그리디 알고리즘을 적용해서 풀면 된다.
    각 위치에서 자신의 좌측을 보면서, 어디서 왔을 때 현재 값이 가장 커질지를 확인하면 된다.
    예를들어 현재 F 위치라고 보면, 일단 D와 E는 이전값이 될 수 없다.
    그럼 좌측으로 한칸 전에서 올 수 있는건 C 뿐이다.
    다음은 2칸전을 보자. A와 B 둘 다 어차피 2칸이 떨어져서 상관없다고 생각할 수 있지만,
    조금 더 최적화해보면,
    어차피 2칸전에 B를 선택했다면, 자동으로 C-F로 오게된다. 이미 C는 볼 것이므로 중복체크가 된다.
    반면에 A는 의미가 있는데 A를 선택하면 F를 선택하기 위해 C와 D는 포기해야 하기 때문이다.
    따라서 F 위치만 봤을 때 그리디하게 최대값은 F + max(A, C) 라고 볼 수 있다.
    마찬가지로 E 위치에서는 E + max(B, D) 이다.
    이걸 ArrayIndexOut.. 이 나지 않도록 적절히 처리해주면 (전 그냥 미리 이전에 2열의 더미값을 두었음) 된다.
    최종적으로 답은 맨 마지막 열에 있는 두 값중 큰 값이다.
    --
    추가로 이 문제부터 input, output 을 '속도면에서' 최대한 최적화 해보았다.
    byte 단위로 읽어서 직접 처리하는 input, 그리고 StringBuilder에 붙여서 한번에 출력하기가 들어가 있다.
    실제로 이 커밋멘트 올린 시점 기준으로 자바 기준 최소시간의 코드이다.

---
+++ 09400/BOJ_9465.java
@@ -0,0 +1,60 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        initFI();
+        int tc = nextInt();
+        StringBuilder sb = new StringBuilder();
+        while (tc-->0) {
+            int n = nextInt();
+
+            // input
+            int[][] arr = new int[2][n+2];
+            for (int i = 0; i < 2; i++) for (int j = 2; j < n+2; j++) arr[i][j] = nextInt();
+
+            // solution
+            for (int j = 2; j < n+2; j++) {
+                arr[0][j] += Math.max(arr[1][j-2], arr[1][j-1]);
+                arr[1][j] += Math.max(arr[0][j-2], arr[0][j-1]);
+            }
+            sb.append(Math.max(arr[0][n+1], arr[1][n+1])).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+
+    
+    // Fast IO
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
