317b5cc32196b0ea085876c6ca5f2c1ee6397033
Se-young Hwang <nahwasa@gmail.com>
2021-08-13 19:18:28 +0900

    한글
    아스키코드로 영문이나 숫자 조정하는 것과 마찬가지로 하면 됨.

---
+++ 11200/BOJ_11282.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char c = '가';
+        System.out.println((char)(c + n-1));
+    }
+}
