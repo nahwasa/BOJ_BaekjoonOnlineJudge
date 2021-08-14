d02356d6822d4dca2f93f646b5434f3c265108b1
Se-young Hwang <nahwasa@gmail.com>
2021-08-14 20:58:41 +0900

    수도요금
    그냥 수학문제!
    주의점은 p-c가 0 이하라면 별도 처리 해야함. (p가 c 이하일 동안은 b만 들어가므로)

---
+++ 10700/BOJ_10707.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int p = Integer.parseInt(br.readLine());
+        System.out.println(Math.min(a*p, b+(p-c<0?0:p-c)*d));
+    }
+}
