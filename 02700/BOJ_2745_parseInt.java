f81031907eed0491101942b81fa2fae71a2a4d3f
Se-young Hwang <nahwasa@gmail.com>
2021-08-19 19:48:41 +0900

    진법 변환
    직접 푼거 말고 이번엔 자바에서 제공하는 함수 써서 계산한 코드임.
    별다른 설명은 필요없음.

---
+++ 02700/BOJ_2745_parseInt.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(st.nextToken(), Integer.parseInt(st.nextToken())));
+    }
+}
