0ca1c5e9d166923cedbaf3c73172291a48dd8bde
Se-young Hwang <nahwasa@gmail.com>
2021-10-02 20:25:59 +0900

    이동하기
    해설은 블로그 nahwasa.com에 작성함.

---
+++ 11000/BOJ_11048.java
@@ -0,0 +1,54 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class Main {
+    private static int tripleMax(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+
+    private static void solution() throws Exception {
+        int n = nextInt();
+        int m = nextInt();
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = nextInt() + tripleMax(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        System.out.println(dp[n][m]);
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        solution();
+    }
+
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
