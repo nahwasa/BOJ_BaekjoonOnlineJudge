9f13879ea9d25fd27bece8f57bcd7001f147c7f3
Se-young Hwang <nahwasa@gmail.com>
2021-10-20 23:01:44 +0900

    선 그리기
    1~10000까지 배열에 모든 선을 일단 놓으면서 존재하는 위치를 체크하고, 최종적으로 전체 배열을 보며 놓여있는 위치라면 길이로 세면 됨.

---
+++ 16300/BOJ_16396.java
@@ -0,0 +1,54 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class Main {
+    private static void solution() throws Exception {
+        int[] arr = new int[10001];
+        int n = nextInt();
+        while (n-->0) {
+            int a = nextInt();
+            int b = nextInt();
+            for (int i = a; i < b; i++)
+                arr[i] = 1;
+        }
+        int answer = 0;
+        for (int i = 0; i < 10001; i++) {
+            answer += arr[i];
+        }
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        solution();
+    }
+
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
