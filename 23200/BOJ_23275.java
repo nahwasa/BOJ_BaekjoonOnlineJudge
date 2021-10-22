05e74d64e9f1611c63cb0f73691fcd3008bbfc33
Se-young Hwang <nahwasa@gmail.com>
2021-10-22 20:58:56 +0900

    Knot Knowledge
    일반적인 해싱 문제

---
+++ 23200/BOJ_23275.java
@@ -0,0 +1,58 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class Main {
+    private static void solution() throws Exception {
+        int n = nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+        }
+
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < n-1; i++) {
+            hs.add(nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            if (!hs.contains(arr[i])) {
+                System.out.println(arr[i]);
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        solution();
+    }
+
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
