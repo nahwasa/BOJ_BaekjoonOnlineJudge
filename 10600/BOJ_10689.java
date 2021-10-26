261173ef396b146e5dc3f7615c026209465656ce
Se-young Hwang <nahwasa@gmail.com>
2021-10-26 19:44:35 +0900

    Hamza
    해쉬셋에 계속 넣다가, 마지막으로 해쉬셋 사이즈가 증가한 위치가 답임!

---
+++ 10600/BOJ_10689.java
@@ -0,0 +1,64 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class Main {
+    private static StringBuilder answer = new StringBuilder();
+    private static void print(int tc, int ans) {
+        answer.append('C').append('a').append('s').append('e').append(' ').append(tc).append(':').append(' ').append(ans).append('\n');
+    }
+    private static void flush() { System.out.println(answer); }
+
+    private static void solution() throws Exception {
+        int t = nextInt();
+        for (int tc = 1; tc <= t; tc++) {
+            HashSet<Integer> hs = new HashSet<>();
+            int n = nextInt();
+            int tmp = 0;
+            int ans = 0;
+            for (int i = 1; i <= n; i++) {
+                hs.add(nextInt());
+                if (hs.size() > tmp) {
+                    ans = i;
+                    tmp = hs.size();
+                }
+            }
+            print(tc, ans);
+        }
+        flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        solution();
+    }
+
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
