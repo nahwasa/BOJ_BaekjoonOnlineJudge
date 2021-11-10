078d516995320db4e5f248e124063214b1fd246e
Se-young Hwang <nahwasa@gmail.com>
2021-11-10 20:22:49 +0900

    Not A + B
    a와 b가 1보다 크므로 a+b의 최소값은 2이다. 따라서 테스트 케이스 갯수만큼 1만 출력해주면 된다.

---
+++ 23500/BOJ_23530.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) System.out.println(1);
+    }
+}
