a1012733e1dbed857b6b39e725a715a812ca5ce3
Se-young Hwang <nahwasa@gmail.com>
2021-11-11 23:48:54 +0900

    저울
    해설은 nahwasa.com 블로그에!

---
+++ 02400/BOJ_2437.java
@@ -0,0 +1,55 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    private void solution() throws Exception {
+        int n = nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = nextInt();
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int cur : arr) {
+            if (cur >= sum+2) break;
+            sum+=cur;
+        }
+        System.out.println(sum+1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        new Main().solution();
+    }
+
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    private static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    private static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
