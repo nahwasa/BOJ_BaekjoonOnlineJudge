300753d17fce9a830a4f7d284bb4a529ef95af06
Se-young Hwang <nahwasa@gmail.com>
2021-11-13 11:45:30 +0900

    꿍 가라사대
    문자열 기본 처리 함수들만 알면 풀 수 있음

---
+++ 11000/BOJ_11094.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (n-->0) {
+            String s = br.readLine();
+            if (s.indexOf("Simon says") == 0) {
+                sb.append(s.substring(10)).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
