979b21c20afe7378314ce9030dbe006084550db6
Se-young Hwang <nahwasa@gmail.com>
2021-11-21 14:12:06 +0900

    Sklep
    값 압축 후(N이 최대 100만개이므로 A를 10^9까지에서 100만 이하로 압추 가능) 배열로 카운팅 혹은 해시맵을 통한 카운팅
    위 코드는 해시를 통한 카운팅!
    들어온 순서만 해시에 없던 키값일 경우 별도로 관리해주면 됨

---
+++ 08600/BOJ_8641.java
@@ -0,0 +1,66 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Main extends FastInput {
+    private void solution() throws Exception {
+        int n = nextInt();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        ArrayList<Integer> order = new ArrayList<>();
+        while (n-->0) {
+            int a = nextInt();
+            int k = nextInt();
+            if (!hm.containsKey(a)) {
+                order.add(a);
+                hm.put(a, k);
+                continue;
+            }
+            hm.put(a, hm.get(a) + k);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(order.size()).append('\n');
+        for (int i = 0; i < order.size(); i++) {
+            sb.append(order.get(i)).append(' ').append(hm.get(order.get(i))).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        new Main().solution();
+    }
+}
+
+class FastInput {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
