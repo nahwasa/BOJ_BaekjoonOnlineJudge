34d37376fda0cb81518e437951f5865378da7f79
Se-young Hwang <nahwasa@gmail.com>
2021-12-06 22:58:29 +0900

    설탕 배달
    좀 더 효율적으로 변경

--- 02800/BOJ_2839.java
+++ 02800/BOJ_2839.java
@@ -1,36 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-public class Main{
-	
-	public static void main(String[] args) {
-		final int bag5kg = 5;
-		final int bag3kg = 3;
-		final int INIT_RESULT = Integer.MAX_VALUE;
-		
-		Scanner sc = new Scanner(System.in);
-		int weight = sc.nextInt();
-		
-		int result = INIT_RESULT;		
-		
-		for (int i = 0; i <= weight / bag5kg; i++) {
-			int num = 0;
-			int tmp = weight - bag5kg * i;
-			num += i;
-			
-			if (tmp % bag3kg == 0)
-				num += tmp / bag3kg;
-			else
-				continue;
-			
-		
-			if (result > num)
-				result = num;
-			
-		}
-		
-		if (result == INIT_RESULT)
-			result = -1;
-		
-		System.out.println(result);
-	}
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int base = n/5+1;
+        while (base-->0) {
+            if ((n-base*5)%3==0) {
+                System.out.println(base+(n-base*5)/3);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
 }
