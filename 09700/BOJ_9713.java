45a7ae592193cc36b3cf76a5ab3eca2a0ec1d18a
Se-young Hwang <nahwasa@gmail.com>
2021-12-13 23:32:02 +0900

    Sum of Odd Sequence
    효율적으로 변경. n번째 홀수에 대한 등차수열의 합 = n^2 (등차수열 합 공식에 홀수 넣어보면 나옴)

--- 09700/BOJ_9713.java
+++ 09700/BOJ_9713.java
@@ -1,26 +1,19 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
-		int[] arr = new int[50];
-		int num = 3;
-		arr[0] = 1;
-		for (int i = 1; i < 50; i++) {
-			arr[i] = arr[i-1] + num;
-			num+=2;
-		}
-		int N = Integer.parseInt(br.readLine());
-		while (N-->0) {
-			bw.write(arr[Integer.parseInt(br.readLine())/2] + "\n");
-		}
-		bw.flush();
-		br.close();
-		bw.close();		
-	}
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (tc-->0) {
+            int cur = Integer.parseInt(br.readLine()) / 2 + 1;
+            sb.append(cur*cur).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
 }
