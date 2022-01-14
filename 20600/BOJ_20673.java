949bd8d151ea0e5dac2eb34778c646ccb7679d09
Se-young Hwang <nahwasa@gmail.com>
2022-01-14 16:59:39 +0900

    Covid-19
    재채점 후 틀려서 수정함

--- 20600/BOJ_20673.java
+++ 20600/BOJ_20673.java
@@ -7,7 +7,7 @@ public class Main {
        int p = Integer.parseInt(br.readLine());
        int q = Integer.parseInt(br.readLine());
        if (p <= 50 && q <= 10) System.out.println("White");
-       else if (q >= 30) System.out.println("Red");
+       else if (q > 30) System.out.println("Red");
        else System.out.println("Yellow");
    }
 }
