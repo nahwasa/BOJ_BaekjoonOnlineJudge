9781e6bfa8b8c38d946cb2bb19d18cb7976263e0
Se-young Hwang <nahwasa@gmail.com>
2022-01-31 13:19:42 +0900

    뒤집기
    해설은 nahwasa.com 블로그 입니다!

---
+++ 01400/BOJ_1439.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        StringTokenizer st1 = new StringTokenizer(s, "0");
+        StringTokenizer st0 = new StringTokenizer(s, "1");
+        System.out.println(Math.min(st1.countTokens(), st0.countTokens()));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
