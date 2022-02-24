a47d4ddc816c9a9bb2088eb19f3cc3aefb4e17af
Se-young Hwang <nahwasa@gmail.com>
2022-02-24 17:02:05 +0900

    에너지 드링크
    해설은 nahwasa.com 블로그에!

---
+++ 20100/BOJ_20115.java
@@ -0,0 +1,53 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main extends FastInput {
+    private void solution() throws Exception {
+        int n = nextInt();
+        int max = 0;
+        long sum = 0;
+        while (n-->0){
+            int cur = nextInt();
+            if (cur>max) max = cur;
+            sum+=cur;
+        }
+        System.out.println(max+(sum-max)/2d);
+    }
+
+    public static void main(String[] args) throws Exception {
+        initFI();
+        new Main().solution();
+    }
+}
+
+class FastInput {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
