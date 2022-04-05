8eb940e052887462286a98ab6a4e5a3ae71a16c0
Se-young Hwang <nahwasa@gmail.com>
2022-04-05 14:13:15 +0900

    소수의 연속합
    해설은 nahwasa.com 블로그에!

---
+++ 01600/BOJ_1644.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    ArrayList<Integer> pn = new ArrayList<>();
+
+    private void initPn(int n) {
+        boolean[] isPn = new boolean[n+1];
+        int sqrtN = (int)Math.sqrt(n);
+        for (int i = 3; i <= sqrtN; i+=2) {
+            if (isPn[i]) continue;
+            int base = i+i;
+            while (base <= n) {
+                isPn[base] = true;
+                base+=i;
+            }
+        }
+        pn.add(2);
+        for (int i = 3; i <= n; i+=2) {
+            if (!isPn[i]) pn.add(i);
+        }
+    }
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        initPn(n);
+        int st = -1;
+        int ed = -1;
+        int sum = 0;
+        int cnt = 0;
+        while (st != pn.size()-1 || ed != pn.size()-1) {
+            if (sum < n) {
+                if (ed == pn.size()-1) {
+                    sum -= pn.get(++st);
+                } else {
+                    sum += pn.get(++ed);
+                }
+            }
+            else if (sum >= n)
+                sum-=pn.get(++st);
+
+            if (sum == n) cnt++;
+        }
+        System.out.println(cnt);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
