7dbcca366c38f4ddc6fb72cc837e6b8faf82776a
Se-young Hwang <nahwasa@gmail.com>
2022-05-09 08:10:04 +0900

    주사위 세개
    해설은 nahwasa.com 블로그에!

--- 02400/BOJ_2480.java
+++ 02400/BOJ_2480.java
@@ -6,27 +6,27 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-				
-		if (a == b && b == c) {
-			bw.write((10000 + 1000 * a) + "\n");
-		} else if (a == b || b == c || a == c) {
-			int sameNum = a==b?a : (b==c?b : c);
-			bw.write(1000 + 100 * sameNum + "\n");
-		} else {
-			int maxNum = a>b?a: (b>c?b:c);
-			bw.write(100 * maxNum + "\n");
-		}
-				
-		bw.flush();
-		br.close();
-		bw.close();		
-	}	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a == b && b == c) {
+            bw.write((10000 + 1000 * a) + "\n");
+        } else if (a == b || b == c || a == c) {
+            int sameNum = a==b?a : (b==c?b : c);
+            bw.write(1000 + 100 * sameNum + "\n");
+        } else {
+            int maxNum = Math.max(Math.max(a,b),c);
+            bw.write(100 * maxNum + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
