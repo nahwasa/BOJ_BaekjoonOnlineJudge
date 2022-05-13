e55eef6a60ab213fad3ee55fd42c46a2446eb3ad
Se-young Hwang <nahwasa@gmail.com>
2022-05-13 21:48:33 +0900

    새싹
    해설은 nahwasa.com 블로그에!

---
+++ 25000/BOJ_25083.java
@@ -0,0 +1,13 @@
+public class Main {
+    private void solution() throws Exception {
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
