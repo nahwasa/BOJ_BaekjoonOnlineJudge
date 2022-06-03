701d57ef47a90e21ba65786b039a261513e9144c
Se-young Hwang <nahwasa@gmail.com>
2022-06-03 17:47:32 +0900

    알고리즘 수업 - 너비 우선 탐색 1
    해설은 nahwasa.com 블로그에!

---
+++ 24400/BOJ_24444.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    int[] answer;
+    ArrayList<Integer>[] edges;
+    int idx = 0;
+    int[] v;
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        edges = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) edges[i] = new ArrayList<>();
+        answer = new int[n+1];
+        v = new int[n+1];
+        while (m-->0) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            edges[u].add(v);
+            edges[v].add(u);
+        }
+        for (int i = 1; i <= n; i++) Collections.sort(edges[i]);
+
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(r);
+        int visitCnt = 1;
+        v[r] = visitCnt;
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int next : edges[cur]) {
+                if (v[next]!=0) continue;
+                v[next] = ++visitCnt;
+                q.add(next);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(v[i]).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
