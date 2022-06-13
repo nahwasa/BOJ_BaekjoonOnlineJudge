8c457f14b0ade39cd1a80304d08efc084fef6350
Se-young Hwang <nahwasa@gmail.com>
2022-06-13 19:49:13 +0900

    SASA 모형을 만들어보자
    해설은 nahwasa.com 블로그에!

---
+++ 23800/BOJ_23825.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println(Math.min(n/2, m/2));
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
