42b5ff9fa470bc1822d32add39f0a67d7782bd5d
Se-young Hwang <nahwasa@gmail.com>
2022-06-24 23:04:46 +0900

    녹색거탑
    해설은 nahwasa.com 블로그에!

---
+++ 24700/BOJ_24723.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private final static int MOD = 20000303;
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int calc = 1;
+        while (n-->0) calc*=2;
+      
+        System.out.println(calc);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
