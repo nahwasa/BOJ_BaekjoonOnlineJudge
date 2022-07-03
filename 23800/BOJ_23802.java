62aa7519389cdc7f9a3b8c1c86027ab4a6af70c9
Se-young Hwang <nahwasa@gmail.com>
2022-07-03 23:28:44 +0900

    골뱅이 찍기 - 뒤집힌 ㄱ
    해설은 nahwasa.com 블로그에!

---
+++ 23800/BOJ_23802.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5*n; j++) {
+                sb.append('@');
+            }
+            sb.append('\n');
+        }
+        for (int i = 0; i < 4*n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append('@');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
