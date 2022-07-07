e58826549a1333c0a1a5b8a66a5cc372a9e85f42
Se-young Hwang <nahwasa@gmail.com>
2022-07-07 18:46:56 +0900

    주사위
    해설은 nahwaasa.com 블로그에!

---
+++ 09200/BOJ_9295.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(String.format("Case %d: %d\n", i, a+b));
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
