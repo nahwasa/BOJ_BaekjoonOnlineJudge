c5df4554850e500b48f727385dfd570dc1c67be2
Se-young Hwang <nahwasa@gmail.com>
2022-08-04 18:11:07 +0900

    알고리즘 수업 - 선택 정렬 1
    해설은 nahwas.com 블로그에!

---
+++ 23800/BOJ_23881.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static int N, K, kth1=-1, kth2;
+
+    private void selectionSort(int[] A) {   // A[1..N]을 오름차순 정렬한다.
+        int swapCnt = 0;
+        for (int last = N; last >= 2; last--) {
+            // A[1..last]중 가장 큰 수 A[i]를 찾는다
+            int max = Integer.MIN_VALUE;
+            int i = 0;
+            for (int idx = 1; idx <= last; idx++) {
+                if (max < A[idx]) {
+                    max = A[idx];
+                    i = idx;
+                }
+            }
+
+            // last와 i가 서로 다르면 A[last]와 A[i]를 교환
+            if (last != i) {
+                int tmp = A[last];
+                A[last] = A[i];
+                A[i] = tmp;
+
+                if (++swapCnt == K) {
+                    kth1 = A[i];
+                    kth2 = A[last];
+                    return;
+                }
+            }
+        }
+    }
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N +1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        selectionSort(arr);
+        System.out.println(kth1==-1?-1:(kth1 + " " + kth2));
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
