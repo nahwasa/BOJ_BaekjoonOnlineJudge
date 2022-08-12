366689479afdadf2dbc76d8515c2f4cf858815d0
Se-young Hwang <nahwasa@gmail.com>
2022-08-12 20:33:02 +0900

    구간 합 구하기 (1인덱스 베이스(일반적인) 펜윅 트리)
    해설은 nahwasa.com 블로그에!

---
+++ 02000/BOJ_2042_fenwick_tree.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private long[] bit, arr;
+    private int n;
+
+    private long query(int b, int c) {
+        return getPrefixSum(c) - getPrefixSum(b-1);
+    }
+
+    private long getPrefixSum(int ith) {
+        long answer = 0l;
+        while (ith > 0) {
+            answer += bit[ith];
+            ith -= ith&-ith;
+        }
+        return answer;
+    }
+
+    private void update(int ith, long val) {
+        long diff = val - arr[ith];
+        arr[ith] = val;
+
+        while (ith <= n) {
+            bit[ith] += diff;
+            ith += ith&-ith;
+        }
+    }
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int mk = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+        arr = new long[n+1];
+        bit = new long[n+1];
+        for (int i = 1; i <= n; i++) {
+            update(i, Long.parseLong(br.readLine()));
+        }
+        StringBuilder sb = new StringBuilder();
+        while (mk-->0) {
+            st = new StringTokenizer(br.readLine());
+            switch (Integer.parseInt(st.nextToken())) {
+                case 1: update(Integer.parseInt(st.nextToken()), Long.parseLong(st.nextToken())); break;
+                case 2: sb.append(query(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))).append('\n'); break;
+            }
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
