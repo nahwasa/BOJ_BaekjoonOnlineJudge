117458a34e86752bbddf72f207e95e6a176aa2f3
Se-young Hwang <nahwasa@gmail.com>
2022-08-21 09:56:11 +0900

    영수증
    해설은 nahwasa.com 블로그에!

---
+++ 25300/BOJ_25304.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        while (n-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum+=a*b;
+        }
+        System.out.println(sum==x?"Yes":"No");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
