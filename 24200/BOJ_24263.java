4c418b30d42dc3e2e8949da04c16ad85beab7688
Se-young Hwang <nahwasa@gmail.com>
2022-10-17 19:05:12 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 2
    해설은 nahwasa.com 블로그에!

---
+++ 24200/BOJ_24263.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine()+"\n1");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
