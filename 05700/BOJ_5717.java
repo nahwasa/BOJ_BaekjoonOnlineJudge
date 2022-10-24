d57ad43d509be668ff01ed0eb94b5deae6139a8a
Se-young Hwang <nahwasa@gmail.com>
2022-10-24 19:00:46 +0900

    상근이의 친구들
    해설은 nahwasa.com 블로그에!

---
+++ 05700/BOJ_5717.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a==0 && b==0)
+                break;
+            sb.append(a+b).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
