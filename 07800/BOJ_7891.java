d31c4143a1d8ba0d52afab2d448ae4f47b0374fa
Se-young Hwang <nahwasa@gmail.com>
2022-10-31 21:24:12 +0900

    Can you add this?
    해설은 nahwasa.com 블로그에!

---
+++ 07800/BOJ_7891.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (n-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
