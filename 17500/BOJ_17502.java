2779bd87540ef0cc85dd6af1ae8e2afa57e9cd69
Se-young Hwang <nahwasa@gmail.com>
2022-11-07 22:55:06 +0900

    클레어와 팰린드롬
    해설은 nahwasa.com 블로그에!

---
+++ 17500/BOJ_17502.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char[] arr = br.readLine().toCharArray();
+        for (int i = 0; i < n/2+1; i++) {
+            char cur = 'a';
+            if (arr[i] != '?') cur = arr[i];
+            if (arr[n-i-1] != '?') cur = arr[n-i-1];
+            arr[i] = arr[n-i-1] = cur;
+        }
+        System.out.println(String.valueOf(arr));
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
