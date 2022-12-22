6038ac9e890c9ed3f3dc5a3fe0326f0dc68a9a2c
Se-young Hwang <nahwasa@gmail.com>
2022-12-22 20:26:33 +0900

    Helicopter

---
+++ 14300/BOJ_14337.vb
@@ -0,0 +1,14 @@
+Module Main
+ 
+  Sub Main()
+    Dim s() As String = Nothing
+    Console.WriteLine ("      ===================")
+    Console.WriteLine ("          ____||___")
+    Console.WriteLine ("\ /      /       []\")
+    Console.WriteLine (" X=======           \__")
+    Console.WriteLine ("/ \      \____________|")
+    Console.WriteLine ("            ||  ||")
+    Console.WriteLine ("         \-----------/")
+  End Sub
+ 
+End Module
