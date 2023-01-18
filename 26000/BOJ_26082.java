773c2f557bdaf3174a0460e61becc8eac3cb7f71
Se-young Hwang <nahwasa@gmail.com>
2023-01-18 22:22:28 +0900

    WARBOY
    해설은 nahwasa.com 블로그에!

---
+++ 26000/BOJ_26082.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        System.out.println(c*(b/a)*3);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
