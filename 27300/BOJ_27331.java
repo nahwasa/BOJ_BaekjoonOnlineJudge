cddc5f2ab7b5b41a6adca9e402aeb4daec9f36c7
Se-young Hwang <nahwasa@gmail.com>
2023-02-06 20:00:56 +0900

    2 桁の整数 (Two-digit Integer)
    해설은 nahwasa.com 블로그에!

---
+++ 27300/BOJ_27331.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    public void solution() throws Exception {
+        System.out.println(br.readLine() + br.readLine());
+    }
+}
