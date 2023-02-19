4f9379a442c16755e9b806e8a156939a9c32369f
Se-young Hwang <nahwasa@gmail.com>
2023-02-19 15:53:01 +0900

    Metronome
    해설은 nahwasa.com 블로그에!

---
+++ 27300/BOJ_27389.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    public void solution() throws Exception {
+        System.out.printf("%.2f", 1d*Integer.parseInt(br.readLine()) / 4);
+    }
+}
