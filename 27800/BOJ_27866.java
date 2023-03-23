73600daacf221bb3c27b9936e3d83545363bd2cc
Se-young Hwang <nahwasa@gmail.com>
2023-03-23 18:30:59 +0900

    문자와 문자열

---
+++ 27800/BOJ_27866.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        String s = br.readLine();
+        System.out.println(s.charAt(Integer.parseInt(br.readLine())-1));
+    }
+}
