2887a46f8b87b01132b3567e260af9b3707b819b
Se-young Hwang <nahwasa@gmail.com>
2023-03-25 20:00:48 +0900

    삼각 무늬 - 1
    해설은 nahwasa.com 블로그에!

---
+++ 11300/BOJ_11320.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (tc-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            a*=a;
+            b*=b;
+            int answer = a/b + (a%b==0?0:1);
+            sb.append(answer).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
