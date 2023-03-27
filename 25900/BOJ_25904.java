0089a3e64067450581c028fd41e5ddd45f17f0c1
Se-young Hwang <nahwasa@gmail.com>
2023-03-27 20:52:32 +0900

    안녕 클레오파트라 세상에서 제일가는 포테이토칩
    해설은 nahwasa.com 블로그에!

---
+++ 25900/BOJ_25904.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int idx = 0;
+        while (true) {
+            if (arr[idx] < x) {
+                System.out.println(idx % n + 1);
+                return;
+            }
+            idx++;
+            idx%=n;
+            x++;
+        }
+    }
+}
