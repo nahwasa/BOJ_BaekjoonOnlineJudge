deabdf1db7727cdebba9ebd7b8da2eb44898ea82
Se-young Hwang <nahwasa@gmail.com>
2023-04-19 18:24:51 +0900

    초코바
    해설은 nahwasa.com 블로그에!

---
+++ 27900/BOJ_27959.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a*100<b?"No":"Yes");
+    }
+}
