f6b059640b1f03d9781ccae7013ebd012b1aac7e
Se-young Hwang <nahwasa@gmail.com>
2023-04-25 20:34:58 +0900

    試験 (Exam)

---
+++ 18400/BOJ_18411.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int sum = a+b+c;
+        sum -= Math.min(Math.min(a,b),c);
+        System.out.println(sum);
+    }
+}
