eacb15e581c11289e9ea6fb430f19e02857eca9b
Se-young Hwang <nahwasa@gmail.com>
2023-05-22 16:03:04 +0900

    정보섬의 대중교통

---
+++ 28100/BOJ_28113.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a==b?"Anything":a>b?"Subway":"Bus");
+    }
+}
