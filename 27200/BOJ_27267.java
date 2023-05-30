00713334268426f4e114128795bbe6cad50d7274
Se-young Hwang <nahwasa@gmail.com>
2023-05-30 22:17:24 +0900

    Сравнение комнат

---
+++ 27200/BOJ_27267.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        System.out.println(a*b==c*d ? "E" : a*b>c*d ? "M" : "P");
+    }
+}
