dfba1a5d57e245712b68f7879c386e64dcf4f506
Se-young Hwang <nahwasa@gmail.com>
2023-06-05 11:13:58 +0900

    Лифт

---
+++ 27200/BOJ_27262.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int elv = (n+k-2)*b;
+        int walk = (n-1)*a;
+
+        System.out.println(elv + " " + walk);
+    }
+}
