24c0587bc90e865ad5b4c95a9537761833e5db09
Se-young Hwang <nahwasa@gmail.com>
2023-06-12 21:18:06 +0900

    ПЧЕЛИЧКАТА МАЯ

---
+++ 24300/BOJ_24365.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int avg = (a+b+c)/3;
+        int ans = avg-a;
+        b-=ans;
+        ans += avg-b;
+
+        System.out.println(ans);
+    }
+}
