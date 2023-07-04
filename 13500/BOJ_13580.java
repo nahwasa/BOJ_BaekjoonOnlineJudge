38b151a43d1edb23e18e2de0974baa9d801353f1
Se-young Hwang <nahwasa@gmail.com>
2023-07-04 18:01:09 +0900

    Andando no tempo

---
+++ 13500/BOJ_13580.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] == arr[1] || arr[1] == arr[2] || arr[0] + arr[1] == arr[2] ? 'S' : 'N');
+    }
+}
