4dae56a203d4037d848771ca7a8a8d5ffa23ee7e
Se-young Hwang <nahwasa@gmail.com>
2023-07-31 18:42:40 +0900

    Communication Channels

---
+++ 11100/BOJ_11121.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        int tc = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (tc-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            sb.append(st.nextToken().equals(st.nextToken()) ? "OK" : "ERROR").append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
