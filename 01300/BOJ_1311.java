e4c91c7536f5905b8bcdcb913c28555e6059fbef
Se-young Hwang <nahwasa@gmail.com>
2023-08-03 21:49:37 +0900

    할 일 정하기 1
    해설은 nahwasa.com 블로그에!

---
+++ 01300/BOJ_1311.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+    static final int INF = 20*10000+1;
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[n+1][1<<(n+1)];
+        for (int[] row : dp) Arrays.fill(row, INF);
+        dp[0][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < 1<<(n+1); j++) {
+                if (dp[i-1][j] == INF) continue;
+
+                for (int k = 1; k <= n; k++) {
+                    if ((j&(1<<k)) != 0) continue;
+                    dp[i][j|(1<<k)] = Math.min(dp[i][j|(1<<k)], dp[i-1][j] + arr[i][k]);
+                }
+            }
+
+        }
+
+        System.out.println(dp[n][(1<<(n+1))-2]);
+    }
+}
