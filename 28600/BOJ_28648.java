96623811179c83f1675825cde2590db24918e69e
Se-young Hwang <nahwasa@gmail.com>
2023-08-18 22:29:31 +0900

    Торговый центр

---
+++ 28600/BOJ_28648.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+        while (n-->0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            min = Math.min(min, a+b);
+        }
+
+        System.out.println(min);
+    }
+}
