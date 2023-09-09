bd6e93804ca02938bd1f2765e66759ac948f9479
Se-young Hwang <nahwasa@gmail.com>
2023-09-09 21:50:57 +0900

    Welcome to SMUPC!

---
+++ 29600/BOJ_29699.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    private void solution() throws Exception {
+        int n = Integer.parseInt(br.readLine()) - 1;
+        System.out.println("WelcomeToSMUPC".charAt(n%14));
+    }
+}
