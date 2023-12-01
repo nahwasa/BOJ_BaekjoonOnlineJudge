26e15411b731dbedf08f993aeb142f4b155b83dd
Se-young Hwang <nahwasa@gmail.com>
2023-12-01 17:32:39 +0900

    나이트 게임

---
+++ 16800/BOJ_16884.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+    private static final String KOOSAGA = "koosaga";
+    private static final String CUBELOVER = "cubelover";
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+
+    public void solution() throws Exception {
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-->0) {
+            int cur = Integer.parseInt(br.readLine());
+            sb.append((cur&1)==1?KOOSAGA:CUBELOVER).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
